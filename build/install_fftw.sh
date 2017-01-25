b5145b18e9d16336d1151cdb90c263386500bbf7
Louisvh <Louisvanharten@gmail.com>
2017-01-25 11:06:14 +0100

    fixed fftw error on travis

--- build/install_fftw.sh
+++ build/install_fftw.sh
@@ -12,7 +12,7 @@ else
         wget http://www.fftw.org/fftw-3.3.5.tar.gz
         tar -xzf fftw-3.3.5.tar.gz
         cd fftw-3.3.5
-        echo configuring with: ./configure --enable-shared --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-generic-    simd128 CFLAGS="-fPIC" CPPFLAGS="-fPIC"
+        echo configuring with: ./configure --enable-shared --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 CFLAGS="-fPIC" CPPFLAGS="-fPIC"
         ./configure --enable-shared --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-generic-simd128 CFLAGS="-fPIC" CPPFLAGS="-fPIC" > /dev/null
         echo compiling...
         make > /dev/null
@@ -24,7 +24,7 @@ else
         wget http://www.fftw.org/fftw-3.3.5.tar.gz
         tar -xzf fftw-3.3.5.tar.gz
         cd fftw-3.3.5
-        echo configuring with: ./configure --enable-shared --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-generic-    simd128 CFLAGS="-fPIC" CPPFLAGS="-fPIC"
+        echo configuring with: ./configure --enable-shared --enable-float --enable-sse --enable-sse2 --enable-avx CFLAGS="-fPIC" CPPFLAGS="-fPIC"
         ./configure --enable-shared --enable-float --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-generic-simd128 CXX="g++ -stdlib=libstdc++ -arch x86_64 -fPIC" > /dev/null
         echo compiling...
         make > /dev/null
